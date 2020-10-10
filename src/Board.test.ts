f3f56915f9ba095d6f0d92b127f9091e84636b90
Cody Smith <37386486+oncody@users.noreply.github.com>
2020-10-10 15:17:35 -0600

    adding more tests

--- src/Board.test.ts
+++ src/Board.test.ts
@@ -1,6 +1,7 @@
 import Board from './Board';
 import {Column} from './Column';
 import {Row} from './Row';
+import {Color} from "./Color";
 
 let board = new Board();
 
@@ -67,3 +68,68 @@ test('Board get square g1 has column G', () => {
 test('Board get square h1 has column H', () => {
     expect(board.getSquare(Column.H, Row.ROW_1).getCoordinate().getColumn()).toBe(Column.H);
 });
+
+test('Board get square a1 is black', () => {
+    expect(board.getSquare(Column.A, Row.ROW_1).getColor()).toBe(Color.Black);
+});
+
+test('Board get square a2 is white', () => {
+    expect(board.getSquare(Column.A, Row.ROW_2).getColor()).toBe(Color.White);
+});
+
+test('Board get square a3 is black', () => {
+    expect(board.getSquare(Column.A, Row.ROW_3).getColor()).toBe(Color.Black);
+});
+
+test('Board get square a4 is white', () => {
+    expect(board.getSquare(Column.A, Row.ROW_4).getColor()).toBe(Color.White);
+});
+
+test('Board get square a5 is black', () => {
+    expect(board.getSquare(Column.A, Row.ROW_5).getColor()).toBe(Color.Black);
+});
+
+test('Board get square a6 is white', () => {
+    expect(board.getSquare(Column.A, Row.ROW_6).getColor()).toBe(Color.White);
+});
+
+test('Board get square a7 is black', () => {
+    expect(board.getSquare(Column.A, Row.ROW_7).getColor()).toBe(Color.Black);
+});
+
+test('Board get square a8 is white', () => {
+    expect(board.getSquare(Column.A, Row.ROW_8).getColor()).toBe(Color.White);
+});
+
+test('Board get square b1 is white', () => {
+    expect(board.getSquare(Column.B, Row.ROW_1).getColor()).toBe(Color.White);
+});
+
+test('Board get square b2 is black', () => {
+    expect(board.getSquare(Column.B, Row.ROW_2).getColor()).toBe(Color.Black);
+});
+
+test('Board get square b3 is white', () => {
+    expect(board.getSquare(Column.B, Row.ROW_3).getColor()).toBe(Color.White);
+});
+
+test('Board get square b4 is black', () => {
+    expect(board.getSquare(Column.B, Row.ROW_4).getColor()).toBe(Color.Black);
+});
+
+test('Board get square b5 is white', () => {
+    expect(board.getSquare(Column.B, Row.ROW_5).getColor()).toBe(Color.White);
+});
+
+test('Board get square b6 is black', () => {
+    expect(board.getSquare(Column.B, Row.ROW_6).getColor()).toBe(Color.Black);
+});
+
+test('Board get square b7 is white', () => {
+    expect(board.getSquare(Column.B, Row.ROW_7).getColor()).toBe(Color.White);
+});
+
+test('Board get square b8 is black', () => {
+    expect(board.getSquare(Column.B, Row.ROW_8).getColor()).toBe(Color.Black);
+});
+
