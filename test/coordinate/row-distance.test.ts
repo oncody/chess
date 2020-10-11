f11bc3458db85b3c132aa7f5e494a067addaf0f9
Cody Smith <37386486+oncody@users.noreply.github.com>
2020-10-11 00:50:03 -0600

    adding more row tests

--- test/coordinate/row-distance.test.ts
+++ test/coordinate/row-distance.test.ts
@@ -30,3 +30,38 @@ test('Row distance between a1 and h3 is 2', () => {
     let coordinatePair = new CoordinatePair(a1, h3);
     expect(coordinatePair.distanceBetweenRows()).toBe(2);
 });
+
+test('Row distance between a1 and h4 is 3', () => {
+    let a1 = new Coordinate(Column.A, Row.ROW_1);
+    let h4 = new Coordinate(Column.H, Row.ROW_4);
+    let coordinatePair = new CoordinatePair(a1, h4);
+    expect(coordinatePair.distanceBetweenRows()).toBe(3);
+});
+
+test('Row distance between a1 and h5 is 4', () => {
+    let a1 = new Coordinate(Column.A, Row.ROW_1);
+    let h5 = new Coordinate(Column.H, Row.ROW_5);
+    let coordinatePair = new CoordinatePair(a1, h5);
+    expect(coordinatePair.distanceBetweenRows()).toBe(4);
+});
+
+test('Row distance between a1 and h6 is 5', () => {
+    let a1 = new Coordinate(Column.A, Row.ROW_1);
+    let h6 = new Coordinate(Column.H, Row.ROW_6);
+    let coordinatePair = new CoordinatePair(a1, h6);
+    expect(coordinatePair.distanceBetweenRows()).toBe(5);
+});
+
+test('Row distance between a1 and h7 is 6', () => {
+    let a1 = new Coordinate(Column.A, Row.ROW_1);
+    let h7 = new Coordinate(Column.H, Row.ROW_7);
+    let coordinatePair = new CoordinatePair(a1, h7);
+    expect(coordinatePair.distanceBetweenRows()).toBe(6);
+});
+
+test('Row distance between a1 and a8 is 7', () => {
+    let a1 = new Coordinate(Column.A, Row.ROW_1);
+    let a8 = new Coordinate(Column.A, Row.ROW_8);
+    let coordinatePair = new CoordinatePair(a1, a8);
+    expect(coordinatePair.distanceBetweenRows()).toBe(7);
+});
