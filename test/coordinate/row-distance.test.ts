819ed35ea55f6ad384d7f6c62cc924c774d598f6
Cody Smith <37386486+oncody@users.noreply.github.com>
2020-10-11 00:35:55 -0600

    adding more tests

--- test/coordinate/row-distance.test.ts
+++ test/coordinate/row-distance.test.ts
@@ -16,3 +16,17 @@ test('Row distance between a1 and h2 is 1', () => {
     let coordinatePair = new CoordinatePair(a1, h2);
     expect(coordinatePair.distanceBetweenRows()).toBe(1);
 });
+
+test('Row distance between h2 and a1 is 1', () => {
+    let h2 = new Coordinate(Column.H, Row.ROW_2);
+    let a1 = new Coordinate(Column.A, Row.ROW_1);
+    let coordinatePair = new CoordinatePair(h2, a1);
+    expect(coordinatePair.distanceBetweenRows()).toBe(1);
+});
+
+test('Row distance between a1 and h3 is 2', () => {
+    let a1 = new Coordinate(Column.A, Row.ROW_1);
+    let h3 = new Coordinate(Column.H, Row.ROW_3);
+    let coordinatePair = new CoordinatePair(a1, h3);
+    expect(coordinatePair.distanceBetweenRows()).toBe(2);
+});
