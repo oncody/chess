fd90117dc47e0fcb52addbfe185011492be8ec84
Cody Smith <37386486+oncody@users.noreply.github.com>
2020-10-27 21:21:30 -0600

    adding orthogonal test

--- test/coordinate/orthogonal.test.ts
+++ test/coordinate/orthogonal.test.ts
@@ -4,20 +4,28 @@ import Coordinate from '../../src/board/Coordinate';
 import {coordinates} from '../../src/board/CoordinateIterator';
 
 test('a1 orthogonal test', () => {
-  let corner = new Coordinate(Column.A, Row.ROW_1);
-  let adjacentSquares = [
-    new Coordinate(Column.A, Row.ROW_1),
-    new Coordinate(Column.B, Row.ROW_1),
-    new Coordinate(Column.B, Row.ROW_2)
-  ];
+  let a1 = new Coordinate(Column.A, Row.ROW_1);
 
-  // for (let coordinate of coordinates()) {
-  //   if (adjacentSquares.some(adjacentSquare => adjacentSquare.areEqual(coordinate))) {
-  //     expect(corner.isAdjacent(coordinate)).toBe(true);
-  //     expect(coordinate.isAdjacent(corner)).toBe(true);
-  //   } else {
-  //     expect(corner.isAdjacent(coordinate)).toBe(false);
-  //     expect(coordinate.isAdjacent(corner)).toBe(false);
-  //   }
-  // }
+  let squaresOrthogonal = 0;
+  let squaresNonOrthogonal = 0;
+
+  for (let coordinate of coordinates()) {
+    let isMatch = coordinate.getRow() === Row.ROW_1 || coordinate.getColumn() === Column.A;
+
+    if (isMatch) {
+      expect(a1.isOrthogonal(coordinate)).toBe(true);
+      expect(coordinate.isOrthogonal(a1)).toBe(true);
+      squaresOrthogonal++;
+    } else {
+      expect(a1.isOrthogonal(coordinate)).toBe(false);
+      expect(coordinate.isOrthogonal(a1)).toBe(false);
+      squaresNonOrthogonal++;
+    }
+  }
+
+  // one row is 8. one column is 8. so you would expect this number to be 16
+  // but there is one element that overlaps. it matches this row and column but
+  // we should only count it once
+  expect(squaresOrthogonal).toBe(15);
+  expect(squaresOrthogonal + squaresNonOrthogonal).toBe(64);
 });
