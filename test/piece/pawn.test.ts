4f7f5c0d41744021d0e8a67822297bce4f67739b
Cody Smith <37386486+oncody@users.noreply.github.com>
2020-11-01 22:39:10 -0700

    pawn test

--- test/piece/pawn.test.ts
+++ test/piece/pawn.test.ts
@@ -1,7 +1,17 @@
 import Coordinate from '../../src/board/Coordinate';
 import {Column} from '../../src/board/Column';
 import {Row} from '../../src/board/Row';
-import {coordinates} from '../../src/board/CoordinateIterator';
+import Board from '../../src/board/Board';
+import Pawn from '../../src/piece/Pawn';
+import {Color} from '../../src/Color';
+import Player from '../../src/Player';
 
-test('white pawn can move forward twice on first move', () => {}
-);
+test('white pawn can move forward twice on first move', () => {
+  let board = new Board();
+  let whitePlayer = new Player(Color.White);
+  let whitePawn = new Pawn(Color.White);
+  let a2 = new Coordinate(Column.A, Row.ROW_2);
+  let a4 = new Coordinate(Column.A, Row.ROW_4);
+  board.getSquare(a2).addPiece(whitePawn);
+  expect(board.movePiece(whitePlayer, a2, a4)).toBeUndefined();
+});
