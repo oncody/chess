fc856c02d7c2d986342b0eb7e5a69817fc26862b
Cody Smith <37386486+oncody@users.noreply.github.com>
2020-11-01 23:02:23 -0700

    more pawn tests

--- test/piece/pawn.test.ts
+++ test/piece/pawn.test.ts
@@ -15,3 +15,33 @@ test('white pawn can move forward twice on first move', () => {
   board.getSquare(a2).addPiece(whitePawn);
   expect(board.movePiece(whitePlayer, a2, a4)).toBeUndefined();
 });
+
+test('black pawn can move forward twice on first move', () => {
+  let board = new Board();
+  let blackPlayer = new Player(Color.Black);
+  let blackPawn = new Pawn(Color.Black);
+  let a7 = new Coordinate(Column.A, Row.ROW_7);
+  let a5 = new Coordinate(Column.A, Row.ROW_5);
+  board.getSquare(a7).addPiece(blackPawn);
+  expect(board.movePiece(blackPlayer, a7, a5)).toBeUndefined();
+});
+
+test('white pawn can move forward once on first move', () => {
+  let board = new Board();
+  let whitePlayer = new Player(Color.White);
+  let whitePawn = new Pawn(Color.White);
+  let a2 = new Coordinate(Column.A, Row.ROW_2);
+  let a3 = new Coordinate(Column.A, Row.ROW_3);
+  board.getSquare(a2).addPiece(whitePawn);
+  expect(board.movePiece(whitePlayer, a2, a3)).toBeUndefined();
+});
+
+test('black pawn can move forward once on first move', () => {
+  let board = new Board();
+  let blackPlayer = new Player(Color.Black);
+  let blackPawn = new Pawn(Color.Black);
+  let a7 = new Coordinate(Column.A, Row.ROW_7);
+  let a6 = new Coordinate(Column.A, Row.ROW_6);
+  board.getSquare(a7).addPiece(blackPawn);
+  expect(board.movePiece(blackPlayer, a7, a6)).toBeUndefined();
+});
